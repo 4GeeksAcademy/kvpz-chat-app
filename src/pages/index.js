776549dc06daf9fe6b2204989cc57f4bb5199de6
kevinpazsoldan <152312603+kevinpazsoldan@users.noreply.github.com>
2024-03-07 00:13:45 +0000

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import { CounterContainer } from "@/components/CounterContainer";
 import { Col, Container, Row } from "react-bootstrap";
-import { FaClock } from "react-icons/fa";
 
 export default function Home() {
   return (
